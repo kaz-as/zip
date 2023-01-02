77dfc5612b0ebb75f0547125d681a7946dc5bee3
Aleksandr Kazantsev <kaz.a.s@ya.ru>
2023-01-02 12:03:33 +0300

    generate by swagger

--- go.mod
+++ go.mod
@@ -3,17 +3,36 @@ module github.com/kaz-as/zip
 go 1.19
 
 require (
+	github.com/go-openapi/errors v0.20.3
+	github.com/go-openapi/loads v0.21.2
+	github.com/go-openapi/runtime v0.25.0
+	github.com/go-openapi/spec v0.20.7
+	github.com/go-openapi/strfmt v0.21.3
+	github.com/go-openapi/swag v0.22.3
+	github.com/go-openapi/validate v0.22.0
 	github.com/gorilla/mux v1.8.0
 	github.com/ilyakaznacheev/cleanenv v1.4.2
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/rs/zerolog v1.28.0
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
 )
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/go-openapi/analysis v0.21.4 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ restapi/configure_zip.go
@@ -0,0 +1,97 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/kaz-as/zip/restapi/operations"
+)
+
+//go:generate swagger generate server --target ..\..\zip --name Zip --spec ..\docs\swagger.yml --principal interface{} --exclude-main
+
+func configureFlags(api *operations.ZipAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.ZipAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.BinConsumer = runtime.ByteStreamConsumer()
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.BinProducer = runtime.ByteStreamProducer()
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.GetFilesHandler == nil {
+		api.GetFilesHandler = operations.GetFilesHandlerFunc(func(params operations.GetFilesParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.GetFiles has not yet been implemented")
+		})
+	}
+	if api.GetFilesUploadHandler == nil {
+		api.GetFilesUploadHandler = operations.GetFilesUploadHandlerFunc(func(params operations.GetFilesUploadParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.GetFilesUpload has not yet been implemented")
+		})
+	}
+	if api.PostFilesHandler == nil {
+		api.PostFilesHandler = operations.PostFilesHandlerFunc(func(params operations.PostFilesParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.PostFiles has not yet been implemented")
+		})
+	}
+	if api.PostFilesUploadHandler == nil {
+		api.PostFilesUploadHandler = operations.PostFilesUploadHandlerFunc(func(params operations.PostFilesUploadParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.PostFilesUpload has not yet been implemented")
+		})
+	}
+	if api.PostFilesZipHandler == nil {
+		api.PostFilesZipHandler = operations.PostFilesZipHandlerFunc(func(params operations.PostFilesZipParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.PostFilesZip has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
